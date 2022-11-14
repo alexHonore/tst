d7965e7b95f7e75f6b63959c5ff71decf2cf23c9
alexHonore <98293124+alexHonore@users.noreply.github.com>
2022-11-14 14:16:48 -0500

    m

---
+++ atoibase.c
@@ -0,0 +1,37 @@
+// return the decimal value of the number [c] from its base
+int	base(int c, int base)
+{
+	char *str = "0123456789abcdef";
+	char *str2 = "0123456789ABCDEF";
+	int  i = 0;
+	while (i < base)
+	{
+		if (c == str[i] || c == str2[i])
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+int ft_atoi_base(const char *str, int str_base)
+{
+	int nb = 0;
+	int neg = 1;
+	int	i = 0;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '-')
+	{
+		neg *= -1;
+		i++;
+	}
+	else if (str[i] == '+')
+		i++;
+	while (base(str[i], str_base) != -1)
+	{
+		nb *= str_base;
+		nb += base(str[i], str_base);
+		i++;
+	}
+	return (nb * neg);
+}
